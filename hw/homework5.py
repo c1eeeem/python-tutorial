573cb253a2e47ae116fec82660525b6f39337c58
Merkulov Daniil <kitukawa0@gmail.com>
2025-10-05 18:24:29 +0300

    начали программу распознавания покерных комбинаций

--- hw/homework5.py
+++ hw/homework5.py
@@ -1,8 +1,24 @@
-def is_safe_bridge(bridge: str):
-    bridge = bridge.strip()
-    return " " not in bridge
+#def is_safe_bridge(bridge: str):
+#    bridge = bridge.strip()
+#    return " " not in bridge
 
-print(is_safe_bridge("####"))     # True
-print(is_safe_bridge("## ####"))  # False
-print(is_safe_bridge("#"))        # True
+#print(is_safe_bridge("####"))     # True
+#print(is_safe_bridge("## ####"))  # False
+#print(is_safe_bridge("#"))        # True
 
+
+deck = ['Ч2', 'П2', 'К2', 'Б2', 'Ч3', 'П3', 'К3', 'Б3']
+board = []
+
+
+results = {'стрит-флеш': 0,
+           'каре': 0}
+
+random.choice()
+board.append()
+
+
+hand1 = []
+hand2 = []
+
+#дописать колоду,
